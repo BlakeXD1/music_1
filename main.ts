40675b5425745e6295d56092888230a0e0192515
BlakeXD1 <131759982+BlakeXD1@users.noreply.github.com>
2023-04-25 10:17:10 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.AB, function () {
+    music.playMelody("D G F C E D E F ", 240)
+    music.playMelody("C F C E D E F - ", 240)
+    music.playMelody("C G F C E C E F ", 240)
+    music.playMelody("C F A G F G - - ", 240)
+})
+basic.forever(function () {
+	
+})
